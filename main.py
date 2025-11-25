2c43c103e7ff20eae20752fe8765406e172099cc
Lera <bodnar4yk>
2025-11-25 15:23:54 +0100

    tasks_while_for

---
+++ main.py
@@ -0,0 +1,66 @@
+###task1###
+#  a=10
+# if a % 2 == 0:
+#     print('var is even')
+# else:
+#     print('a is odd')
+
+
+###tast2###
+# a=10
+# b=20
+# if a>b:
+#     print('a>b')
+# elif a==b:
+#     print('a==b')
+# else:
+#     print('a<b')
+
+###task3###
+# a=int(input("enter number"))
+# if a>0:
+#     print('a is positive number')
+# elif a<0:
+#     print('a is negative number')
+# else:
+#     print('a is zero')
+
+###task4###
+# a=int(input('enter a '))
+# b=int(input('enter b '))
+# c=int(input('enter c '))
+# if a>b and a>c:
+#     print("a the greather number ")
+# elif b>a and b>c:
+#     print ("b is the greather number ")
+# elif c>a and c>b:
+#     print ('c is the greather number ')
+
+###task5###
+# a=str(input("enter text "))
+# if "Python" in a:
+#     print("Python exist in a")
+# else:
+#     print("Python NOT exist in a")
+
+###task6###
+# x=1
+# while x<=10:
+#     print(x)
+#     x+=1
+
+###task7###
+# for i in range(1,11):
+#     print(i)
+
+###task8###
+# x=1
+# while x<=20:
+#     if x%2==0:
+#         print(x)
+#     x+=1
+
+###task9###
+for i in range (1,20):
+    if i%2!=0:
+        print(i)
